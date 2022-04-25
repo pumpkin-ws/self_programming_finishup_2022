432244571e42dd30ead6b72f7087f8b9b33cad6d
pumpkin-ws <pumpkin_wang@foxmail.com>
2022-04-25 17:00:40 +0800

    test eigen and redis

--- multirobot/eigen-test.cpp
+++ multirobot/eigen-test.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
+    /* For a rotation vector, the norm is the angle of rotation,
+        and the unit vector is the direction of rotation.
+     */
     Eigen::Vector3d vd{1, 1, 1};
     printf("---------------\n");
     std::cout << vd.norm() << std::endl;
