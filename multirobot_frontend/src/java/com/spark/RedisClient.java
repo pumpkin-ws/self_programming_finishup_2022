c7d8688b09e59f8565f993efc398988945668fde
pumpkin-ws <pumpkin_wang@foxmail.com>
2022-04-26 10:58:45 +0800

    some java update

--- multirobot_frontend/src/java/com/spark/RedisClient.java
+++ multirobot_frontend/src/java/com/spark/RedisClient.java
@@ -1,7 +1,10 @@
 package com.spark;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisClient {
     public static String IP = "127.0.0.1";
     public static int PORT = 6379;
-    
+    public static JedisPool jedisPool= new JedisPool(new JedisPoolConfig(), IP, PORT);
+    Publisher
 }
